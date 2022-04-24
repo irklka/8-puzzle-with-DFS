01f1ee4d86678e8bfe1f8dfc0bb1d1c05630275f
kapitan <62106615+irklka@users.noreply.github.com>
2022-04-24 23:56:24 +0400

    changed matrix initialization fort DeserializeMatrix method.

--- dfss/Program.cs
+++ dfss/Program.cs
@@ -104,11 +104,10 @@ namespace DFSGame
         /// <returns>Jagged array int[][],</returns>
         public int[][] DeserializeMatrix(string str)
         {
-            int[][] newMat = new int[3][]
-            {
-                new int[]{0,0,0},
-                new int[]{0,0,0},
-                new int[]{0,0,0}
+            var newMat = new int[3][]{
+                new int[3],
+                new int[3],
+                new int[3]
             };
 
             for (int i = 0; i < 9; i++)
